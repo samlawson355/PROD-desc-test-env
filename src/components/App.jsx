d8c76f2764d1b0f3665cd8eb2e9cbe718043d90c
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 17:15:32 -0600

    routes work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,37 +12,61 @@ class App extends React.Component {
   }
 
   selectProduct(event) {
-      console.log(`FILE: App.jsx selectProduct() | event.target.value: \n`, event.target.value);
-      // console.log(event.target);
-    //  http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/70
-      axios.get('http://localhost:3000/70', )
-          .then(data => {
-              console.log(`FILE: api.js () | data: \n`, data);
-              this.setState({currentProduct: data.data[0]})
-          })
-          .catch(error => {
-              console.log(`FILE: api.js () | ERROR: \n`, error);
-          });
-    // axios
-    //   .get({
-    //     headers: {
-    //       "Access-Control-Allow-Origin": "*"
-    //     },
-    //     // url: `http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/${event.target.value}`
-    //     // url: `localhost:3000/${event.target.value}`
-    //     url: `localhost:3000/70`
-    //   })
-    //     .then(data => {
-    //         console.log(`FILE: App.jsx () | data: \n`, data);
-    //         // this.setState({
-    //         //     currentProduct: data.data[0]
-    //         // })
-    //     })
-    //     .catch(error => {
-    //     	console.log(`FILE: App.jsx () | ERROR: \n`, error);
-    //     })
-
+    axios
+      .get(
+        `http://node-express-env.pd2fd7phmh.us-east-2.elasticbeanstalk.com/api/${event.target.value}`,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .catch(console.log)
+      .then(data =>
+        this.setState({
+          currentProduct: data.data[0]
+        })
+      );
   }
+  // selectProduct(event) {
+  //   console.log(
+  //     `FILE: App.jsx selectProduct() | event.target.value: \n`,
+  //     event.target.value
+  //   );
+  //   // console.log(event.target);
+  //   //http://localhost:3000/70
+  //   //  http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/70
+  //   axios
+  //     .post(
+  //       "http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/products",
+  //       { productID: 70 }
+  //     )
+  //     .then(data => {
+  //       console.log(`FILE: api.js () | data: \n`, data);
+  //       this.setState({ currentProduct: data.data[0] });
+  //     })
+  //     .catch(error => {
+  //       console.log(`FILE: api.js () | ERROR: \n`, error);
+  //     });
+  //   // axios
+  //   //   .get({
+  //   //     headers: {
+  //   //       "Access-Control-Allow-Origin": "*"
+  //   //     },
+  //   //     // url: `http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/${event.target.value}`
+  //   //     // url: `localhost:3000/${event.target.value}`
+  //   //     url: `localhost:3000/70`
+  //   //   })
+  //   //     .then(data => {
+  //   //         console.log(`FILE: App.jsx () | data: \n`, data);
+  //   //         // this.setState({
+  //   //         //     currentProduct: data.data[0]
+  //   //         // })
+  //   //     })
+  //   //     .catch(error => {
+  //   //     	console.log(`FILE: App.jsx () | ERROR: \n`, error);
+  //   //     })
+  //   //}
 
   // selectProduct(event) {
   //   axios({
@@ -55,17 +79,15 @@ class App extends React.Component {
   //   );
   // }
 
-
-
   render() {
-       console.log("test");
-          return !this.state.currentProduct ? (
-            <button onClick={this.selectProduct} value="70">
-              Click for products
-            </button>
-          ) : (
-            <Description currentProduct={this.state.currentProduct} />
-          );
+    console.log("test");
+    return !this.state.currentProduct ? (
+      <button onClick={this.selectProduct} value="70">
+        Click for products
+      </button>
+    ) : (
+      <Description currentProduct={this.state.currentProduct} />
+    );
   }
 }
 
