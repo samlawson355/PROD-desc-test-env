ee8b4f4ebdcbe570079dc1a782d79c06b6a3678c
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-12 10:46:21 -0600

    added react-dev script, bundle commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@ import App from "./components/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(<div>test</div>, document.getElementById("root"));
-
-// "scripts": {
-//   "start": "webpack -d --watch & live-server --port=9000 --watch=dist --open=dist",
-//   "build": "webpack -p"
-// },
