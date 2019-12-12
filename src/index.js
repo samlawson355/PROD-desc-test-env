3cdac798a4c47d3b6ad0bc195f87ac9477fb5553
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-12 08:43:00 -0600

    test deploy, changed package.json for this try by removing npm start

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(<div>test</div>, document.getElementById("root"));
 
+// "scripts": {
+//   "start": "webpack -d --watch & live-server --port=9000 --watch=dist --open=dist",
+//   "build": "webpack -p"
+// },
